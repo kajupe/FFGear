a1a90d3352623ce1aa66de59aefeaaeea4897c5e
Kaj <kajupemail@gmail.com>
2025-07-19 02:15:59 +0200

    characterstocking support & backface fix
    Added support for characterstocking.shpk
    
    Also I apparently got confused about the backface thing before because now it's very much broken so I turned that back to how it used to be (?). Need to double check it at some point.
    
    The main shader now has more parameters exposed, mainly alpha override controls and IOR (set to 1.35 by default instead of the previous 1.5)

--- FFGear/mtrl_handler.py
+++ FFGear/mtrl_handler.py
@@ -16,7 +16,7 @@ logger.setLevel(logging.INFO)
 ###¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤###
 
 class MaterialFlags(Flag):
-    RenderBackfaces = 0x1
+    HideBackfaces = 0x1
     Bit1 = 0x2
     Bit2 = 0x4
     Bit3 = 0x8
